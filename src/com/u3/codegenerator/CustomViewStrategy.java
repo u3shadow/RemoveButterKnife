d5c1cd3a978b535d2b2360d5033ffeaf2e203561
u3shadow <u3shadow@gmail.com>
2018-12-22 15:53:46 +0800

    add custom view support

--- src/com/u3/codegenerator/CustomViewStrategy.java
+++ src/com/u3/codegenerator/CustomViewStrategy.java
@@ -14,21 +14,21 @@ public class CustomViewStrategy implements GenCodeStrategy{
     }
     @Override
     public void genFindView(PsiClass mClass, PsiElementFactory mFactory) {
-        PsiMethod onCreateView = mClass.findMethodsByName("onCreateView", false)[0];
-        for (PsiStatement statement : onCreateView.getBody().getStatements()) {
-            String returnValue = statement.getText();
-            if (returnValue.contains("R.layout")||returnValue.contains("LayoutInflater.from")) {
-                String viewName = returnValue.trim().split(" ")[1]+".";
-                for (int i = 0; i < code.size(); i++) {
-                    StringBuffer buffer = new StringBuffer(code.get(i));
-                    int num = buffer.indexOf(")");
-                    buffer.insert(num+1,viewName);
-                    try {
-                        statement.addAfter(mFactory.createStatementFromText(
-                                buffer.toString(), mClass), statement);
-                    }catch (Exception e1){}
+        PsiMethod[] methods = mClass.getAllMethods();
+        for (PsiMethod method:methods) {
+            for (PsiStatement statement : method.getBody().getStatements()) {
+                String returnValue = statement.getText();
+                if (returnValue.contains("R.layout") || returnValue.contains("LayoutInflater.from(context).inflate")) {
+                    for (int i = 0; i < code.size(); i++) {
+                        StringBuffer buffer = new StringBuffer(code.get(i));
+                        try {
+                            statement.addAfter(mFactory.createStatementFromText(
+                                    buffer.toString(), mClass), statement);
+                        } catch (Exception e1) {
+                        }
+                    }
+                    break;
                 }
-                break;
             }
         }
     }
--- src/com/u3/codegenerator/GenCodeStrategy.java
+++ src/com/u3/codegenerator/GenCodeStrategy.java
@@ -5,8 +5,12 @@ import com.intellij.psi.PsiElementFactory;
 
 public interface GenCodeStrategy {
     default void genCode(PsiClass mClass, PsiElementFactory mFactory){
-        genFindView(mClass,mFactory);
-        genOnClick(mClass,mFactory);
+        try {
+            genFindView(mClass, mFactory);
+            genOnClick(mClass, mFactory);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
     void genFindView(PsiClass mClass, PsiElementFactory mFactory);
     void genOnClick(PsiClass mClass, PsiElementFactory mFactory);
