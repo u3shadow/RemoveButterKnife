a219cca3562177dd836639ed303d557c3bf0b8aa
u3shadow <u3shadow@gmail.com>
2018-12-25 14:02:24 +0800

    add use of delete onclick

--- src/com/u3/codegenerator/AndroidCodeWriter.java
+++ src/com/u3/codegenerator/AndroidCodeWriter.java
@@ -4,8 +4,10 @@ import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.search.EverythingGlobalScope;
+import com.u3.filechains.ClickMehtod;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xiaolei on 2016/6/17.
@@ -14,12 +16,14 @@ public class AndroidCodeWriter extends  WriteCommandAction.Simple {
     PsiClass mClass;
     private PsiElementFactory mFactory;
     List<String> code;
+    Map<ClickMehtod,List<String>> clickMap;
     Project mProject;
-    public AndroidCodeWriter(Project project, PsiFile file, PsiClass psiClass, List<String> code, PsiElementFactory mFactory) {
+    public AndroidCodeWriter(Project project, PsiFile file, PsiClass psiClass, List<String> code, PsiElementFactory mFactory, Map<ClickMehtod,List<String>> clickMap) {
         super(project, file);
         mClass = psiClass;
         this.code = code;
         this.mFactory = mFactory;
+        this.clickMap = clickMap;
         mProject = project;
     }
 
@@ -28,13 +32,13 @@ public class AndroidCodeWriter extends  WriteCommandAction.Simple {
         GenCodeContext codeContext = new GenCodeContext(mClass, mFactory);
         String type = mClass.getSuperClassType().toString();
             if (type.contains("Activity")){
-                codeContext.setStrategy(new ActivityStrategy(code));
+                codeContext.setStrategy(new ActivityStrategy(code,clickMap));
             }else if (type.contains("Fragment")) {
-                codeContext.setStrategy(new FragmentStrategy(code));
+                codeContext.setStrategy(new FragmentStrategy(code,clickMap));
             }else if (type.contains("ViewHolder")||type.contains("Adapter<ViewHolder>")) {
-                codeContext.setStrategy(new AdapterStrategy(code));
+                codeContext.setStrategy(new AdapterStrategy(code,clickMap));
             }else {
-                codeContext.setStrategy(new CustomViewStrategy(code));
+                codeContext.setStrategy(new CustomViewStrategy(code,clickMap));
             }
             codeContext.executeStrategy();
     }
