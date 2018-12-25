2b65c2a8829e02ff25fbc477db6193189cd9339f
u3shadow <u3shadow@gmail.com>
2018-12-25 14:00:52 +0800

    change base interface to base class

--- src/com/u3/codegenerator/GenCodeStrategy.java
+++ src/com/u3/codegenerator/GenCodeStrategy.java
@@ -3,19 +3,28 @@ package com.u3.codegenerator;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiStatement;
+import com.u3.filechains.ClickMehtod;
 
 import java.util.List;
+import java.util.Map;
 
-public interface GenCodeStrategy {
-    default void genCode(PsiClass mClass, PsiElementFactory mFactory){
+public abstract class GenCodeStrategy {
+    protected List<String> code;
+    protected Map<ClickMehtod,List<String>> clickMap;
+
+    public GenCodeStrategy(List<String> code, Map<ClickMehtod,List<String>> clickMap){
+        this.code = code;
+        this.clickMap = clickMap;
+    }
+    protected void genCode(PsiClass mClass, PsiElementFactory mFactory){
         try {
-            genFindView(mClass, mFactory);
             genOnClick(mClass, mFactory);
+            genFindView(mClass, mFactory);
         }catch (Exception e){
             e.printStackTrace();
         }
     }
-    default void insertFindViewCode(PsiClass mClass, PsiElementFactory mFactory, PsiStatement statement, List<String> code) {
+    protected void insertFindViewCode(PsiClass mClass, PsiElementFactory mFactory, PsiStatement statement, List<String> code) {
         for (int i = code.size() - 1; i >= 0; i--) {
             StringBuffer buffer = new StringBuffer(code.get(i));
             try {
@@ -26,6 +35,34 @@ public interface GenCodeStrategy {
             }
         }
     }
-    void genFindView(PsiClass mClass, PsiElementFactory mFactory);
-    void genOnClick(PsiClass mClass, PsiElementFactory mFactory);
+
+    protected void insertOnclickCode(PsiClass mClass, PsiElementFactory mFactory, PsiStatement statement, String code) {
+        try {
+            statement.addAfter(mFactory.createStatementFromText(
+                    code, mClass), statement);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    protected String getOnClickCode(StringBuilder methodString, String id) {
+        return "findViewById("+id+").setOnClickListener(new View.OnClickListener() {\n" +
+                " @Override\n" +
+                " public void onClick(View v){\n"+
+                methodString.toString()+
+                "}"+
+                "});";
+    }
+
+    protected StringBuilder getMethodInvokeString(ClickMehtod method) {
+        StringBuilder methodString = new StringBuilder();
+        if (method.isHaveArg()){
+            methodString.append(method.getName()+"(("+method.getArgType()+")"+"v);");
+        }else{
+            methodString.append(method.getName()+"();");
+        }
+        return methodString;
+    }
+    abstract void genFindView(PsiClass mClass, PsiElementFactory mFactory);
+    abstract void genOnClick(PsiClass mClass, PsiElementFactory mFactory);
 }
